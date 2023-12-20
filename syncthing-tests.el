5354740381c6fc3b5fa46a232abd1c8ab7bc5fb9
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2023-12-20 19:04:57 +0200

    [FIX] Flooring bug for scaling

--- syncthing-tests.el
+++ syncthing-tests.el
@@ -11,6 +11,89 @@
     (unless (or (string-match-p (regexp-quote "*Messages*") (buffer-name buf)))
       (kill-buffer buf))))
 
+(ert-deftest syncthing-calc-scale ()
+  "Ensure `syncthing--scale-bytes' calculates units properly."
+  (syncthing-cleanup)
+  (should (string= (syncthing--scale-bytes 0 0) "0B"))
+  (should (string= (syncthing--scale-bytes 0 1) "0.0B"))
+  (should (string= (syncthing--scale-bytes 0 2) "0.00B"))
+  (should (string= (syncthing--scale-bytes 0 3) "0.000B"))
+
+  (should (string= (syncthing--scale-bytes 10 0) "10B"))
+  (should (string= (syncthing--scale-bytes 10 1) "10.0B"))
+  (should (string= (syncthing--scale-bytes 10 2) "10.00B"))
+  (should (string= (syncthing--scale-bytes 10 3) "10.000B"))
+
+  (should (string= (syncthing--scale-bytes 100 0) "100B"))
+  (should (string= (syncthing--scale-bytes 100 1) "100.0B"))
+  (should (string= (syncthing--scale-bytes 100 2) "100.00B"))
+  (should (string= (syncthing--scale-bytes 100 3) "100.000B"))
+
+  (should (string= (syncthing--scale-bytes 1000 0) "1000B"))
+  (should (string= (syncthing--scale-bytes 1000 1) "1000.0B"))
+  (should (string= (syncthing--scale-bytes 1000 2) "1000.00B"))
+  (should (string= (syncthing--scale-bytes 1000 3) "1000.000B"))
+
+  (should (string= (syncthing--scale-bytes 1024 0) "1KiB"))
+  (should (string= (syncthing--scale-bytes 1024 1) "1.0KiB"))
+  (should (string= (syncthing--scale-bytes 1024 2) "1.00KiB"))
+  (should (string= (syncthing--scale-bytes 1024 3) "1.000KiB"))
+
+  (should (string= (syncthing--scale-bytes 1123 0) "1KiB"))
+  (should (string= (syncthing--scale-bytes 1123 1) "1.1KiB"))
+  (should (string= (syncthing--scale-bytes 1123 2) "1.10KiB"))
+  (should (string= (syncthing--scale-bytes 1123 3) "1.097KiB"))
+
+  (should (string= (syncthing--scale-bytes 1567 0) "1KiB"))
+  (should (string= (syncthing--scale-bytes 1567 1) "1.5KiB"))
+  (should (string= (syncthing--scale-bytes 1567 2) "1.53KiB"))
+  (should (string= (syncthing--scale-bytes 1567 3) "1.530KiB"))
+
+  (should (string= (syncthing--scale-bytes 1048576 0) "1MiB"))
+  (should (string= (syncthing--scale-bytes 1048576 1) "1.0MiB"))
+  (should (string= (syncthing--scale-bytes 1048576 2) "1.00MiB"))
+  (should (string= (syncthing--scale-bytes 1048576 3) "1.000MiB"))
+
+  (should (string= (syncthing--scale-bytes 1261129 0) "1MiB"))
+  (should (string= (syncthing--scale-bytes 1261129 1) "1.2MiB"))
+  (should (string= (syncthing--scale-bytes 1261129 2) "1.20MiB"))
+  (should (string= (syncthing--scale-bytes 1261129 3) "1.203MiB"))
+
+  (should (string= (syncthing--scale-bytes 2455489 0) "2MiB"))
+  (should (string= (syncthing--scale-bytes 2455489 1) "2.3MiB"))
+  (should (string= (syncthing--scale-bytes 2455489 2) "2.34MiB"))
+  (should (string= (syncthing--scale-bytes 2455489 3) "2.342MiB"))
+
+  (should (string= (syncthing--scale-bytes 1073741824 0) "1GiB"))
+  (should (string= (syncthing--scale-bytes 1073741824 1) "1.0GiB"))
+  (should (string= (syncthing--scale-bytes 1073741824 2) "1.00GiB"))
+  (should (string= (syncthing--scale-bytes 1073741824 3) "1.000GiB"))
+
+  (should (string= (syncthing--scale-bytes 1416247867 0) "1GiB"))
+  (should (string= (syncthing--scale-bytes 1416247867 1) "1.3GiB"))
+  (should (string= (syncthing--scale-bytes 1416247867 2) "1.32GiB"))
+  (should (string= (syncthing--scale-bytes 1416247867 3) "1.319GiB"))
+
+  (should (string= (syncthing--scale-bytes 3847751263 0) "3GiB"))
+  (should (string= (syncthing--scale-bytes 3847751263 1) "3.6GiB"))
+  (should (string= (syncthing--scale-bytes 3847751263 2) "3.58GiB"))
+  (should (string= (syncthing--scale-bytes 3847751263 3) "3.583GiB"))
+
+  (should (string= (syncthing--scale-bytes 1099511627776 0) "1024GiB"))
+  (should (string= (syncthing--scale-bytes 1099511627776 1) "1024.0GiB"))
+  (should (string= (syncthing--scale-bytes 1099511627776 2) "1024.00GiB"))
+  (should (string= (syncthing--scale-bytes 1099511627776 3) "1024.000GiB"))
+
+  (should (string= (syncthing--scale-bytes 1590446354641 0) "1481GiB"))
+  (should (string= (syncthing--scale-bytes 1590446354641 1) "1481.2GiB"))
+  (should (string= (syncthing--scale-bytes 1590446354641 2) "1481.22GiB"))
+  (should (string= (syncthing--scale-bytes 1590446354641 3) "1481.219GiB"))
+
+  (should (string= (syncthing--scale-bytes 6029426229121 0) "5615GiB"))
+  (should (string= (syncthing--scale-bytes 6029426229121 1) "5615.3GiB"))
+  (should (string= (syncthing--scale-bytes 6029426229121 2) "5615.34GiB"))
+  (should (string= (syncthing--scale-bytes 6029426229121 3) "5615.341GiB")))
+
 (ert-deftest syncthing-run-customize ()
   "Run `customize-variable' on missing API token."
   (let (called args)
