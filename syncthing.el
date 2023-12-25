0c933caff40c78ab535fd59f66a49011a5077e52
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2023-12-25 04:10:13 +0200

    [ADD] Folding all with `<backtab>`/shift+`TAB`

--- syncthing.el
+++ syncthing.el
@@ -546,6 +546,8 @@ scan it is at.")
     ;; custom handler for RET / widget input handler
     (define-key map (kbd "RET") #'syncthing--newline)
     (define-key map (kbd "SPC") #'syncthing--newline)
+    (define-key map (kbd "TAB") #'syncthing--newline)
+    (define-key map (kbd "<backtab>") #'syncthing--tab)
     (define-key map (kbd "?") #'describe-bindings)
     (define-key map (kbd "n") #'next-line)
     (define-key map (kbd "p") #'previous-line)
@@ -613,6 +615,43 @@ Argument POS Incoming EVENT position."
     (if button
 	    (widget-apply-action button event)
       (error "You can't edit this part of the Syncthing buffer"))))
+
+(defun syncthing--tab (pos &optional event)
+  "TAB handler.
+Argument POS Incoming EVENT position."
+  (interactive "@d")
+  (syncthing-trace)
+  (let* ((data (syncthing-server-data syncthing-server))
+         (folders (alist-get 'folders data))
+         (devices (alist-get 'devices data))
+         (fold-folders (syncthing-buffer-fold-folders syncthing-buffer))
+         (fold-devices (syncthing-buffer-fold-devices syncthing-buffer))
+         (skip-folders (syncthing-buffer-skip-fold-folders syncthing-buffer))
+         (skip-devices (syncthing-buffer-skip-fold-devices syncthing-buffer)))
+    (if (eq (length skip-folders) (length folders))
+        (progn (setf skip-folders nil)
+               (setf fold-folders nil)
+               (dolist (folder folders)
+                 (push (alist-get 'id folder) fold-folders)))
+      (setf skip-folders nil)
+      (setf fold-folders nil)
+      (dolist (folder folders)
+        (push (alist-get 'id folder) skip-folders)))
+    (setf (syncthing-buffer-fold-folders syncthing-buffer) fold-folders
+          (syncthing-buffer-skip-fold-folders syncthing-buffer) skip-folders)
+
+    (if (eq (length skip-devices) (length devices))
+        (progn (setf skip-devices nil)
+               (setf fold-devices nil)
+               (dolist (device devices)
+                 (push (alist-get 'deviceID device) fold-devices)))
+      (setf skip-devices nil)
+      (setf fold-devices nil)
+      (dolist (device devices)
+        (push (alist-get 'deviceID device) skip-devices)))
+    (setf (syncthing-buffer-fold-devices syncthing-buffer) fold-devices
+          (syncthing-buffer-skip-fold-devices syncthing-buffer) skip-devices))
+  (syncthing--draw syncthing-server))
 
 (defun syncthing--title (text)
   "Format TEXT as title."
