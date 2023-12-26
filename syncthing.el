975eb466d0d81605938cf7ee647d4eb428424905
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2023-12-26 18:16:11 +0200

    [REF] Split network code to separate file

--- syncthing.el
+++ syncthing.el
@@ -41,45 +41,9 @@
 (require 'syncthing-state)
 (require 'syncthing-keyboard)
 (require 'syncthing-common)
+(require 'syncthing-network)
 
 ;; private/helper funcs
-(defun syncthing--ping (server)
-  "Check whether we can use the API at SERVER with TOKEN."
-  (syncthing-trace)
-  (let ((url-request-method "GET")
-        (url-request-extra-headers
-         `(("X-Api-Key" . ,(syncthing-server-token server))))
-        (url-show-status (null syncthing-no-upstream-noise)))
-    (ignore url-request-method url-request-extra-headers)
-    (condition-case-unless-debug nil
-        (with-temp-buffer
-          (url-insert-file-contents
-           (format "%s/rest/system/ping" (syncthing-server-url server))))
-      (file-error (error "Failed to authenticate, check the token!")))))
-
-(defun syncthing--request (method url token &rest data)
-  "Send authenticated HTTP request to Syncthing REST API.
-Argument METHOD HTTP method/verb.
-Argument URL API to call.
-Optional argument DATA Data to send.
-Argument TOKEN API token."
-  (syncthing-trace)
-  (let ((url-request-method method)
-        (url-request-data data)
-        (url-request-extra-headers `(("X-Api-Key" . ,token)))
-        (url-show-status (null syncthing-no-upstream-noise)))
-    (ignore url-request-method method
-            url-request-data data
-            url-request-extra-headers)
-    (condition-case-unless-debug nil
-        (with-temp-buffer
-          (url-insert-file-contents url)
-          (json-parse-buffer :object-type 'alist
-                             :array-type 'list
-                             :null-object nil
-                             :false-object nil))
-      (file-error (error "Failed to handle response for %s" url)))))
-
 (defun syncthing--draw-folders-header (&optional &key before after)
   "Draw folder header with optional BEFORE and AFTER separator."
   (syncthing-trace)
