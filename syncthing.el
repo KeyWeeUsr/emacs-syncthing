5dbb9516f346c390bd9488da52cb4c4b6dda470d
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2024-12-11 00:45:25 +0100

    [ADD] Version bump

--- syncthing.el
+++ syncthing.el
@@ -4,7 +4,7 @@
 
 ;; Author: Peter Badida <keyweeusr@gmail.com>
 ;; Keywords: convenience, syncthing, sync, client, view
-;; Version: 3.0.0
+;; Version: 3.0.1
 ;; Package-Requires: ((emacs "27.1"))
 ;; Homepage: https://github.com/KeyWeeUsr/emacs-syncthing
 
